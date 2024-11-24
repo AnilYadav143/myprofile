91d6777bb5158f905f006f4752c483040d8805db
Anil_Yadav <samyaduvanshi26@gmail.com>
2024-11-24 12:57:49 +0530

    pckage and app file edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import myimg from './assets/images/myimg.jpeg';
-import linkdin from './assets/images/linkdin.png';
-import './App.css'
-import Header from './components/header';
-import Footer from './components/footer';
-
-import About from './pages/about';
-import Contact from './pages/contact';
-import Home from './pages/home';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import './App.css';
 import 'font-awesome/css/font-awesome.min.css';
-
 
 function App() {
   return (
-    <div>
-      <Router>
+    <div className="app-container">
+      {/* Specify basename for routing on GitHub Pages */}
+      <Router basename="/myprofile">
         <Header />
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          {/* Optional: Handle invalid routes */}
+          <Route path="*" element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
-      
-
-      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
